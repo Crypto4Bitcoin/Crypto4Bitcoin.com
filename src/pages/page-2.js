ddf233aee9a769af95d282bb6ed937470aa0db21
Crypto4Bitcoin <67880001+Crypto4Bitcoin@users.noreply.github.com>
2020-12-20 14:22:42 -0500

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link } from "Crypto4Bitcoin"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Unlocking The Truth" />
-    <h1>There Can Only Be 1</h1>
+    <h1>There Can Only Be One</h1>
     <p>At crypto4bitcoin you'll learn everything you need to learn when it comes to cryptocurrency.</p>
     <p>Where to get digital assets. Where to keep digital assets. How to turn your digital assets into cash and more.</p>
     <p>Our goal is to help you stay safe and prevent you from being scammed by untrustworthy people. This new technology is amazing, so our job is to teach you how to keep your money secure and safe. This system is built on trust and there are people in the world who will take full advantage of your trust. We are here to help you on your journey into the crypto community world of trust.</p>
