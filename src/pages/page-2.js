3a04b3a96ff599282cc47d90a411a1bb2f527cc3
Crypto4Bitcoin <67880001+Crypto4Bitcoin@users.noreply.github.com>
2020-12-20 10:48:27 -0500

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,8 +6,12 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Unlocking The Truth" />
     <h1>There Can Only Be 1</h1>
+    <p>At crypto4bitcoin you'll learn everything you need to learn when it comes to cryptocurrency.</p>
+    <p>Where to get digital assets. Where to keep digital assets. How to turn your digital assets into cash and more.</p>
+    <p>Our goal is to help you stay safe and prevent you from being scammed by untrustworthy people. This new technology is amazing, so our job is to teach you how to keep your money secure and safe. This system is built on trust and there are people in the world who will take full advantage of your trust. We are here to help you on your journey into the crypto community world of trust.</p>
+    <p>To learn more, head over to our <a href="https://www.crypto4bitcoin.app/">Crypto Learning Center</a>.</p>
     <p>Welcome To My World</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
