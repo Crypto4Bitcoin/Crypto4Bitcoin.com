cad37024dfa92c7fde4a8b835eec71712c5a746e
Crypto4Bitcoin <67880001+Crypto4Bitcoin@users.noreply.github.com>
2020-12-20 01:46:11 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Crypto4Bitcoin</h1>
+    <p>To jump right into it, you are here to learn about cryptocurrency and bitcoin.
+
+On this site you'll find everything you need to start using cryptocurrency and bitcoins like paper currency.
+Paper currency (aka Cash/Money/fiat currency) is use to exchange for goods and services for hundreds of years now.
+We have exchanged all sorts of things for goods and services, now we
+have something revolutionary and changing the way we exchange money.
+It's called Cryptocurrency.</p>
+    <p>We The People, For The People, By The People...</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
