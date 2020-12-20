9c7b16c832c1a81ac55a1a7680017da6442236ff
Crypto4Bitcoin <67880001+Crypto4Bitcoin@users.noreply.github.com>
2020-12-20 14:10:42 -0500

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "Crypto4Bitcoin"
 
 import Header from "./header"
 import "./layout.css"
