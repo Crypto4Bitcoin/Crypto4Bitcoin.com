44f2d569569f54aaef04d109f263a48ef974840c
Crypto4Bitcoin <67880001+Crypto4Bitcoin@users.noreply.github.com>
2020-12-21 02:44:41 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+<meta name="monetization" content="$coil.xrptipbot.com/OLpZQUSVSmmSgp48l0oMMA"></meta>
 const Header = ({ siteTitle }) => (
   <header
     style={{
